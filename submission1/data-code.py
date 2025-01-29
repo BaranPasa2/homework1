1f812154772f2e43500c67038fdbe5dbcd07e4ec
Baran <34427951+yooshxyz@users.noreply.github.com>
2025-01-28 22:51:34 -0500

    cleaned up a lot

--- submission1/data-code.py
+++ submission1/data-code.py
@@ -2,46 +2,6 @@ import pandas as pd
 import os
 from tabulate import tabulate
 
-""""
-contact_info2015 = pd.read_csv('data/input/CPSC_Contract_Info_2015_01.csv', encoding='latin1')
-year = 2015
-
-#print(contact_info2015.info())
-
-contract_info = pd.read_csv('data/input/CPSC_Contract_Info_2015_01.csv', encoding='latin1', skiprows=1,
-                                names = [    
-                                    "Contract ID", "Plan ID", "organization Type", "Plan Type",
-                                    "Offers Part D",  "SNP Plan", "EGHP", "Organization Name", 
-                                    "Organization Marketing Name", "plan Name", "Parent Organization",
-                                    "Contract Effective Date"], dtype={
-                                        "Contract ID": object, "Plan ID": float, "organization Type": object, 
-                                        "Plan Type": object, "Offers Part D": object,  "SNP Plan": object, "EGHP": object, "Organization Name": object, 
-                                        "Organization Marketing Name": object, "Plan Name": object, "Parent Organization": object,
-                                        "Contract Effective Date": object
-                                    })
-contract_info['id_count'] = contract_info.groupby(['Contract ID', "Plan ID"]).cumcount() + 1
-contract_info = contract_info[contract_info['id_count'] == 1].drop(columns=['id_count'])
-
-enrollment_info= pd.read_csv('data/input/CPSC_Enrollment_Info_2015_01.csv', encoding='latin1', skiprows=1,
-                             names = [
-                                 "Contract Number", "Plan ID", "SSA State County Code", "FIPS State County Code",
-                                 "State", "County", "Enrollment"
-                             ], dtype = {
-                                  "Contract Number": object, "Plan ID": int, "SSA State County Code": int, "FIPS State County Code": float,
-                                 "State": object, "County": object, "Enrollment": object
-                             }, na_values=['*'])
-
-enrollment_info.rename(columns={"Contract Number": "Contract ID"}, inplace=True)
-
-# Do not need to specify year because they are all for the year 2015
-
-plan_data = contract_info.merge(enrollment_info, on=["Contract ID", "Plan ID"], how="left")
-
-for col in ['Plan ID', "Offers Paert D"]
-"""
-
-import pandas as pd
-
 # Load the datasets
 enrollment_file_path = "data/input/CPSC_Enrollment_Info_2015_01.csv"  # Update with your file path
 contract_file_path = "data/input/CPSC_Contract_Info_2015_01.csv"  # Update with your file path
