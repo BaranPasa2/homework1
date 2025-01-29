80816ee9392c813567ae1e469ab295c7b20c4e18
Baran <baran.pasa@emory.edu>
2025-01-29 12:32:14 -0500

    Update test.jl

--- submission2/JuliaFiles/test.jl
+++ submission2/JuliaFiles/test.jl
@@ -8,3 +8,11 @@ Pkg.add("CSV")
 using DataFrames; altContractData = CSV.read("data/input/CPSC_Contract_Info_2015_01.csv", DataFrame; missingstring="")
 using DataFrames; altEnrollmentData = CSV.read("data/input/CPSC_Enrollment_Info_2015_01.csv", DataFrame; missingstring="")
 using DataFrames; altServAreaData = CSV.read("data/input/MA_Cnty_SA_2015_01.csv", DataFrame; missingstring="")
+
+# Checking to see the column header names:
+foreach(println, names(altContractData))
+foreach(println, names(altEnrollmentData))
+#foreach(println, names(altServAreaData))
+
+# merging the datasets:
+innerjoin(altContractData, altEnrollmentData, on=:["Contract ID" => "Contract Number", :"Plan ID"], matchmissing=:notequal)
