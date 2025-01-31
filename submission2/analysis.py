607051e1635a2d6e9c7b383dccb243a756c83df6
Baran <baran.pasa@emory.edu>
2025-01-30 22:44:02 -0500

    Update analysis.py

--- submission2/analysis.py
+++ submission2/analysis.py
@@ -6,3 +6,8 @@ compTotalData = pd.read_csv("data/output/compTotalData.csv")
 #create the tables:
 
 typePlans = compTotalData.pivot_table(index='plan_type', columns='year', values='planid', aggfunc='count')
+
+compTotalDataFin = compTotalData[(compTotalData['snp'] == "No") & (compTotalData['eghp'] == "No") & ((compTotalData['planid'] < 800) | (compTotalData['planid'] >= 900))]
+typePlans = compTotalDataFin.pivot_table(index='plan_type', columns='year', values='avg_enrollment', aggfunc='count')
+
+enrollmentType = compTotalDataFin.pivot_table(index='plan_type', columns='year', values='avg_enrollment', aggfunc='mean')
